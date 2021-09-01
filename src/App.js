1f3a923b2fe87b5ac4ffa98320c6126a19f48368
Chirag Sethiya <chiragsethiya123@gmail.com>
2021-09-01 23:49:08 +0530

    Improving dark mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import { useState } from 'react';
 import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
+
+  const [mode,setMode] = useState('light');
+
+  const toggleMode = ()=>{
+      if(mode ==="light"){
+        setMode('dark') ;
+        document.body.style.backgroundColor = 'grey';
+      }else{
+        setMode('light');
+        document.body.style.backgroundColor = 'white';
+      }
+  }
+
   return (
     <>
-    <Navbar title="TextUtils" aboutText="About TextUtils"/>
+    <Navbar title="TextUtils" aboutText="About TextUtils" mode = {mode} toggleMode={toggleMode}/>
     <div className="container my-3">
-      <TextForm heading="Enter the text to analyze"/> 
+      <TextForm heading="Enter the text to analyze" mode={mode}/> 
       <hr></hr>
       <About></About>
     </div>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -16,6 +16,14 @@ export default function TextForm(props) {
         setText(newText);
     }
 
+    const handleCopyClick = () => {
+        let text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+
+
+    }
+
     const handleOnChange = (event) =>{
         // console.log("On Change");
         setText(event.target.value);
@@ -23,22 +31,23 @@ export default function TextForm(props) {
 
     return (
         <>
-        <div>
+        <div className='container' style={{color:props.mode==='dark'?'white':'black'}}>
             <h1>{props.heading}</h1> 
             
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==="dark"?'white':'black'}} id="myBox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary mx-1" onClick={handleUpClick} >Convert to UpperCase</button>
             <button className="btn btn-primary mx-1" onClick={handleLowClick} >Convert to LowerCase</button>
             <button className="btn btn-primary mx-1" onClick={handleClearClick} >Clear Text</button>
+            <button className="btn btn-primary mx-1" onClick={handleCopyClick} >Copy  Text</button>
         </div>
-        <div className="container my-3">
+        <div className="container my-3" style={{color:props.mode==='dark'?'white':'black'}} >
             <h2>Your Text Summary</h2>
             <p>{text.split(" ").length} Words and {text.length} characters</p>
             <p>{0.008 * text.split("").length} minutes read</p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length>0?text:"Enter text to preview here"}</p>
         </div>
         </>
     )
